8ec3ae7622420f05db54400b1beecff323d5989a
Palmira Romia <kguinera@hotmail.com>
2023-07-05 12:54:16 +0200

    entity creation

---
+++ Account/src/main/java/com/crud/model/entity/Cliente.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.crud.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ * @author Palmira
+ *
+ */
+
+@Entity
+@Table(name="entity")
+public class Cliente {
+	
+
+}
---
+++ Account/src/main/java/com/crud/model/entity/Movimiento.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.crud.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ * @author Palmira
+ *
+ */
+@Entity
+@Table(name="movimiento")
+public class Movimiento {
+
+}
